70f6d494f2018f15797786c0d208ab09dd3845f9
Moisés Franco Gutierrez <you@example.com>
2024-11-25 02:32:21 -0600

    cagadas

---
+++ src/app/meals/edit-meal-model/edit-meal-model.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-meal-model',
+  templateUrl: './edit-meal-model.component.html',
+  styleUrl: './edit-meal-model.component.css'
+})
+export class EditMealModelComponent {
+
+}
