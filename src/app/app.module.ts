e7c8bcd3c3a762c0ecc68f6ff915252c27fbbc33
Moisés Franco Gutierrez <you@example.com>
2024-11-25 10:48:14 -0600

    add: Meals module modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
