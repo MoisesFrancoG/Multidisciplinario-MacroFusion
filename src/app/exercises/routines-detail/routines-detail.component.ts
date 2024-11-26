14587c80e25fbb91bd5ee56c1af351ede5ec3cff
Moisés Franco Gutierrez <you@example.com>
2024-11-26 04:53:58 -0600

    [add]: Rutine gets exercises complete

--- src/app/exercises/routines-detail/routines-detail.component.ts
+++ src/app/exercises/routines-detail/routines-detail.component.ts
@@ -1,23 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RutineService } from '../../services/rutine.service';// Importar el servicio
+import { Ejercicio } from '../../models/Ejercicio';
 
 @Component({
   selector: 'app-routines-detail',
   templateUrl: './routines-detail.component.html',
-  // styleUrl: './routines-detail.component.css'
 })
 export class RoutinesDetailComponent implements OnInit {
-  routine: string = '';
+  routineId: number = 0;
+  ejercicios: Ejercicio[] = [];
   showDropdown = false;
+  title: string = '';
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
-  ){}
+    private router: Router,
+    private rutineService: RutineService 
+  ) {}
 
-    ngOnInit(): void {
-    // Lee el parámetro 'routine' de la URL
-    this.routine = this.route.snapshot.paramMap.get('routine') || 'Rutina no especificada';
+  ngOnInit(): void {
+    this.routineId = +this.route.snapshot.paramMap.get('id')!;
+    this.loadTitle(this.routineId)
+    this.rutineService.getExercicesbyRutine(this.routineId).subscribe({
+      next: (ejercicios) => {
+        this.ejercicios = ejercicios;
+      },
+      error: (err) => {
+        console.error('Error al cargar los ejercicios', err);
+      }
+    });
   }
 
   toggleDropdown() {
@@ -30,8 +42,27 @@ export class RoutinesDetailComponent implements OnInit {
   }
 
   logout() {
-    localStorage.clear(); // Limpiar todos los datos de sesión
-    this.router.navigate(['/']); // Redireccionar a la vista principal
+    localStorage.clear(); 
+    this.router.navigate(['/']); 
   }
 
+  loadTitle(id: number): void {
+    switch(id){
+      case 1:
+        this.title = "EJERCICIOS EN CASA"
+        break
+
+      case 2: 
+        this.title = "CALISTENIA"
+        break
+
+      case 3:
+        this.title = "GYM"
+        break
+
+      case 4:
+        this.title = "FUERZA"
+        break
+    }
+  }
 }
